2d632bb929ac1d84d2683d206bf0ecb5b2378375
taewonb916 <taewonb916@gmail.com>
2021-09-28 20:44:55 +0900

    10th topmenu CSS change

--- js/myfuntion.js
+++ js/myfuntion.js
@@ -1,6 +1,6 @@
-function myFunction() {
-            var menu = document.getElementById("lefttop_menu")
+var menu = document.getElementById("lefttop_menu")
 
+function myFunction() {
         if (matchMedia("(min-width: 320px) and (max-width: 480px)").matches){
             if(menu.style.display =="none") 
                 menu.style.display = "block";
