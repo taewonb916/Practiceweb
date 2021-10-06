d7dd48f95ee16c83abee67017d38851f33165994
taewonb916 <taewonb916@gmail.com>
2021-10-06 23:01:38 +0900

    12th change 4

--- js/myfuntion.js
+++ js/myfuntion.js
@@ -7,3 +7,48 @@ function myFunction() {
                 menu.style.display = "none";
     }
 }
+
+
+function click1() {
+    var dropdown1 = document.getElementsByClassName("dropdown-content_mq1");
+
+    if(dropdown1[0].style.display == "none") 
+        dropdown1[0].style.display = "block";
+    else if(dropdown1[0].style.display == "block") 
+        dropdown1[0].style.display = "none";
+}
+
+function click2() {
+    var dropdown2 = document.getElementsByClassName("dropdown-content_mq2");
+
+    if(dropdown2[0].style.display == "none") 
+        dropdown2[0].style.display = "block";
+    else if(dropdown2[0].style.display == "block") 
+        dropdown2[0].style.display = "none";
+}
+
+function click3() {
+    var dropdown3 = document.getElementsByClassName("dropdown-content_mq3");
+
+    if(dropdown3[0].style.display == "none") 
+        dropdown3[0].style.display = "block";
+    else if(dropdown3[0].style.display == "block") 
+        dropdown3[0].style.display = "none";
+}
+
+function click4() {
+    var dropdown4 = document.getElementsByClassName("dropdown-content_mq4");
+
+    if(dropdown4[0].style.display == "none") 
+        dropdown4[0].style.display = "block";
+    else if(dropdown4[0].style.display == "block") 
+        dropdown4[0].style.display = "none";
+}
+function click5() {
+    var dropdown5 = document.getElementsByClassName("dropdown-content_mq5");
+
+    if(dropdown5[0].style.display == "none") 
+        dropdown5[0].style.display = "block";
+    else if(dropdown5[0].style.display == "block") 
+        dropdown5[0].style.display = "none";
+}
